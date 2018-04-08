7d37512623d916a7f1181724528ca31f3e76df59
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-04-08 10:41:44 +0530

    data.dart completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+     return new MaterialApp(
       title: "The Book App",
       theme: new ThemeData(
         primarySwatch: Colors.blue,
